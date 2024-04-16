87a32de535d3173d2e92d3e8a7903c7b988bf381
651118070 <valentinpola7@gmail.com>
2024-04-16 16:16:55 +0100

    ok

--- server/controllers/FileUpload.js
+++ server/controllers/FileUpload.js
@@ -1,11 +1,18 @@
 import { cloudinary } from "../Utils/fileUpload.js";
-export const upload=async(file)=>{
-    try {
-        const result = await cloudinary.uploader.upload(filePath, { folder: 'uploads' });
-        return result.secure_url;
-      } catch (error) {
-        console.error('Error uploading file to Cloudinary:', error);
-        throw error;
-      }
+export const uploadImage = async (imagePath) => {
 
-}
+    
+    const options = {
+      use_filename: true,
+      unique_filename: false,
+      overwrite: true,
+    };
+
+    try{
+      const result = await cloudinary.uploader.upload(imagePath, options);
+      console.log(result);
+      return result.public_id;
+    } catch (error) {
+      console.error(error);
+    }
+};
