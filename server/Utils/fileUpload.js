89ad69c8995f8fdd52c47cf290337c7697d30a50
651118070 <valentinpola7@gmail.com>
2024-04-16 15:07:13 +0100

    ok

---
+++ server/Utils/fileUpload.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import multer from 'multer'
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/'); 
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+      cb(null, file.fieldname + '-' + uniqueSuffix + '.' + file.originalname.split('.').pop());
+    },
+  });
+export const fileUpload=()=>{
+    
+}
