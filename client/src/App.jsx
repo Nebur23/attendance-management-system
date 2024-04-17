c522e7e49d4a745543dadeb4def7fc556063a3f4
651118070 <valentinpola7@gmail.com>
2024-04-17 15:51:01 +0100

    registration and login ok

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router";
 import Register from "./Pages/Register";
 import Success from "./Pages/Success";
 import Skip from "./Pages/Skip";
-import Login from "./Pages/Login";
+import SignIn from "./Pages/SignIn";
 import Teacher from "./Pages/Teacher";
 import Forgot from "./Pages/Forgot";
 import Presence from "./Pages/Presence";
@@ -15,7 +15,7 @@ export default function App() {
         <Route path='/' element={<Register />} />
         <Route path='/success' element={<Success />} />
         <Route path='/skip' element={<Skip />} />
-        <Route path='/login' element={<Login />} />
+        <Route path='/login' element={<SignIn />} />
         <Route path='/forgot' element={<Forgot />} />
         <Route path='/teacher' element={<Teacher />} />
         <Route path='/presence' element={<Presence />} />
